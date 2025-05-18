59b846224e9f5e8d8288b7e7d0b156cb7490cbee
kenethy <c14230204@john.petra.ac.id>
2025-05-18 20:56:50 +0700

    oke

--- .history/resources/views/components/footer.blade_20250518205632.php
+++ .history/resources/views/components/footer.blade_20250518205632.php
@@ -24,7 +24,7 @@
                                 d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
                         </svg>
                     </a>
-                    <a href="https://tiktok.com/@hartonomotorsidoarjo" target="_blank" aria-label="TikTok Hartono Motor"
+                    <a href="https://tiktok.com/@hartonomotor" target="_blank" aria-label="TikTok Hartono Motor"
                         class="text-white hover:text-red-500 transition-colors">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24"
                             aria-hidden="true">
